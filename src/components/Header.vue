fb3771b6ee0238f1127f50c66d71b8b6a4976e25
MaseJ <mjohnson434@student.cvtc.edu>
2022-09-21 20:31:32 -0500

    Create task managing app

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+		<Button :text="showForm ? 'Close' : 'Add Task'" :color="showForm ? 'red' : 'green'" @btn-click="$emit('toggle-add-task')"/>
+  </header>
+</template>
+
+<script>
+	import Button from "./Button.vue"
+
+  export default {
+		name: "Header",
+		props: ['title', 'showForm'],
+		components: {
+			Button
+		}
+  }
+</script>
+
+<style scoped>
+  header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: 20px;
+  }
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <Task @delete-task="$emit('delete-task', task.id)" :task="task" 
+            @toggle-reminder="$emit('toggle-reminder', task.id)"        
+        />
+    </div>
+</template>
+
+<script>
+    import Task from "./Task.vue"
+    export default {
+        name: "Tasks",
+        props: ["tasks"],
+        components: {
+            Task
+        },
+        emits: ['delete-task', 'toggle-reminder']
+    }
+</script>
